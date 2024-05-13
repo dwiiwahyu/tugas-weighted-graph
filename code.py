cda6feaa244c41a797b74c25f2e6fb25024707f4
Ainulyaqin02 <148976629+Ainulyaqin02@users.noreply.github.com>
2024-05-13 21:31:25 +0700

    Update code.py

--- code.py
+++ code.py
@@ -59,3 +59,64 @@ class WeightedGraph:
         for city in self.cityList:
             distances[city] = float('inf')
 
+distances[source] = 0
+        print (distances)
+        # Initialize list of unvisited cities
+        unvisited_cities = []
+        for city in self.cityList:
+            unvisited_cities.append(city)
+        #unvisited_cities = list(self.cityList.keys())
+        print (unvisited_cities)
+
+        while unvisited_cities:
+            # Find the unvisited city with the smallest distance
+            min_distance = float('inf')
+            closest_city = None
+            #mengiterasi setiap kota yang belum dikunjungi
+            for city in unvisited_cities:
+                #jika jarak kota lebih kecil dari min_distance
+                if distances[city] < min_distance:
+                    min_distance = distances[city]
+                    closest_city = city
+
+            # Remove the closest city from unvisited list
+            unvisited_cities.remove(closest_city)
+
+            # Update distances to neighboring cities
+            for neighbor, weight in self.cityList[closest_city].items():
+                #jika jarak kota terdekat + weight lebih kecil dari jarak kota tetangga
+                distance = distances[closest_city] + weight
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+
+        return distances
+    
+
+# Example usage with Dijkstra's algorithm:
+petaIndonesia = WeightedGraph()
+petaIndonesia.tambahkanKota("BANDUNG")
+petaIndonesia.tambahkanKota("SUBANG")
+petaIndonesia.tambahkanKota("INDRAMAYU")
+petaIndonesia.tambahkanKota("CIREBON")
+petaIndonesia.tambahkanKota("TEGAL")
+petaIndonesia.tambahkanKota("PEMALANG")
+petaIndonesia.tambahkanKota("PURWOKERTO")
+petaIndonesia.tambahkanKota("CILACAP")
+petaIndonesia.tambahkanKota("BANJAR")
+petaIndonesia.tambahkanKota("TASIKMALAYA")
+petaIndonesia.tambahkanKota("KUNINGAN")
+petaIndonesia.tambahkanKota("SUMEDANG")
+
+petaIndonesia.tambahkanJalan("BANDUNG","SUBANG", 53)
+petaIndonesia.tambahkanJalan("BANDUNG","INDRAMAYU", 148)
+petaIndonesia.tambahkanJalan("BANDUNG","CIREBON", 142)
+petaIndonesia.tambahkanJalan("BANDUNG","TEGAL", 211)
+petaIndonesia.tambahkanJalan("BANDUNG","PEMALANG", 257)
+petaIndonesia.tambahkanJalan("BANDUNG","PURWOKERTO", 278)
+petaIndonesia.tambahkanJalan("BANDUNG","CILACAP", 302)
+petaIndonesia.tambahkanJalan("BANDUNG","BANJAR", 148)
+petaIndonesia.tambahkanJalan("BANDUNG","TASIKMALAYA", 116)
+petaIndonesia.tambahkanJalan("BANDUNG","KUNINGAN", 168)
+petaIndonesia.tambahkanJalan("BANDUNG","SUMEDANG", 56)
+petaIndonesia.tambahkanJalan("SUBANG","INDRAMAYU", 81)
+petaIndonesia.tambahkanJalan("SUBANG","CIREBON", 131)
